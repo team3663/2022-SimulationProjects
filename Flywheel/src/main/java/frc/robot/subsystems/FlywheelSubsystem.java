a6d379a2854205bd1d1d0e75a398be6933ea8282
Richard Elmore <richardelmore@users.noreply.github.com>
2022-06-24 17:39:02 -0700

    Initial commit

---
+++ Flywheel/src/main/java/frc/robot/subsystems/FlywheelSubsystem.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class FlywheelSubsystem extends SubsystemBase {
+
+  private double targetSpeed = 0;
+
+  public FlywheelSubsystem() {}
+
+  public double setTargetSpeed(double speed)
+  {
+    targetSpeed = speed;
+
+    return targetSpeed;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
