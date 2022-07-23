b914a0cf58a4b5e95012dd9f244fb4fbc859e5f5
Yvonne Guan <yvonneguan70@gmail.com>
2022-07-22 19:53:57 -0700

    clean up flywheel

--- Flywheel/src/main/java/frc/robot/Robot.java
+++ Flywheel/src/main/java/frc/robot/Robot.java
@@ -1,14 +1,11 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
 
   private RobotContainer robotContainer;
-  private Command autonomousCommand;
-
 
   @Override
   public void robotInit() {
@@ -18,14 +15,5 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
-  }
-
-  @Override
-  public void autonomousInit() {
-    autonomousCommand = robotContainer.getAutonomousCommand();
-
-    if (autonomousCommand != null) {
-      autonomousCommand.schedule();
-    }
   }
 }
