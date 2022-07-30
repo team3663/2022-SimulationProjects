6ddd913dd74d61488524a70e3429806696c5c95b
Yvonne Guan <yvonneguan70@gmail.com>
2022-07-29 19:08:43 -0700

    arm delay resolved!

--- Arm/src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ Arm/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
@@ -23,22 +19,26 @@ public class ArmSubsystem extends SubsystemBase {
   private final SingleJointedArmSim sim;
   private final WPI_TalonFX motor;
 
-  private static final double kP = 0.1;
+  private static final double kP = 0.1; // 0.075202;
   private static final double kI = 0;
   private static final double kD = 0;
   private static final double kF = 0;
   private static final int SLOT_INDEX = 0;
   private static final int PRIMARY_PID = 0;
   private static final int TIMEOUT = 20;
-  private static final double ANGLE_TOLERANCE = Units.degreesToRadians(2);
+  private static final double ANGLE_TOLERANCE = Units.degreesToRadians(0.5);
 
   private static final double GEARING = 10;
   private static final double ARM_LENGTH = 0.5;
   private static final double MIN_ANGLE = 0;
   private static final double MAX_ANGLE = Math.PI * 2;
-  private static final double MASS = 5;
+  private static final double MASS = 15;
   private static final double MOMENT_OF_INERTIA = MASS * ARM_LENGTH * ARM_LENGTH;
 
+  private static final DCMotor MOTOR = DCMotor.getFalcon500(2);
+  private static final double kV = 1. / MOTOR.KvRadPerSecPerVolt / GEARING;
+  private static final double kA = MOTOR.rOhms * MOMENT_OF_INERTIA / GEARING / MOTOR.KtNMPerAmp;
+  
   private static final double SENSOR_POSITION_FACTOR = 2048.0 * GEARING / (2 * Math.PI);
   private static final double SENSOR_VELOCITY_FACTOR = SENSOR_POSITION_FACTOR / 10.0;
 
@@ -52,18 +52,11 @@ public class ArmSubsystem extends SubsystemBase {
     motor.config_kI(SLOT_INDEX, kI);
     motor.config_kD(SLOT_INDEX, kD);
     motor.config_kF(SLOT_INDEX, kF);
-    motor.selectProfileSlot(SLOT_INDEX, PRIMARY_PID);
-    motor.setSelectedSensorPosition(0, PRIMARY_PID, TIMEOUT);
 
     motor.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, TIMEOUT);
 
-    motor.configNominalOutputForward(0);
-    motor.configNominalOutputReverse(0);
-    motor.configPeakOutputForward(1);
-    motor.configPeakOutputReverse(-1);
-
-    motor.configMotionCruiseVelocity(Units.degreesToRadians(10) * SENSOR_VELOCITY_FACTOR);
-    motor.configMotionAcceleration(Units.degreesToRadians(20) * SENSOR_VELOCITY_FACTOR);
+    motor.configMotionCruiseVelocity(Units.degreesToRadians(90) * SENSOR_VELOCITY_FACTOR);
+    motor.configMotionAcceleration((12. - kV * Units.degreesToRadians(90)) / kA * SENSOR_VELOCITY_FACTOR);
 
     sim = new SingleJointedArmSim(DCMotor.getFalcon500(2), GEARING, MOMENT_OF_INERTIA, ARM_LENGTH, MIN_ANGLE, MAX_ANGLE, MASS, false);
 
@@ -75,6 +68,13 @@ public class ArmSubsystem extends SubsystemBase {
   }
 
   public void setTargetAngle(double targetAngle) {
+    if (targetAngle > MAX_ANGLE) {
+      targetAngle = MAX_ANGLE;
+    }
+    else if (targetAngle < 0) {
+      targetAngle = 0;
+    }
+
     this.targetAngle = targetAngle;
   }
 
@@ -106,7 +106,6 @@ public class ArmSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    motor.set(TalonFXControlMode.Position, getTargetAngle() * SENSOR_POSITION_FACTOR);
-    // motor.set(TalonFXControlMode.PercentOutput, 0.1);
+    motor.set(TalonFXControlMode.MotionMagic, getTargetAngle() * SENSOR_POSITION_FACTOR);
   }
 }
